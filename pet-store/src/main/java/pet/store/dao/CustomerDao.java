cb0f81f1eff3a2587342ccb3a197ff2906563fd6
vivienross1 <145514057+vivienross1@users.noreply.github.com>
2024-01-25 13:23:30 -0800

    Add files via upload

---
+++ pet-store/src/main/java/pet/store/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package pet.store.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pet.store.entity.Customer;
+
+
+public interface CustomerDao extends JpaRepository<Customer, Long> {
+
+}
